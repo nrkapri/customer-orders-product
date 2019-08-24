67a9cd8a4348a33ea99819bf8eceb4a9c30e83d3
Nayan Kapri <nrkapri@gmail.com>
2019-08-24 21:45:33 +0000

    .

--- first-service/deployment/kubernetes/commands.sh
+++ first-service/deployment/kubernetes/commands.sh
@@ -1,16 +1,29 @@
+#!/bin/bash
+echo "Kubernetes updates ....."
+
 #delete services
+echo "Deleting services for first-service"
 kubectl delete service first-service
+
 #delete deployments
+echo "Deleting deployment for first-service"
 kubectl delete deployment first-service
+
 #delete configmap
+echo "Deleting configmap for first-service"
 kubectl delete configmap first-service
 
 #apply configmap
+echo "Creating  configmap for first-service"
 kubectl apply -f configmap.yaml
 kubectl get configmap first-service
+
 #apply deployment
-kubectl apply -f deployment.yml
+echo "Creating deployment for first-service"
+kubectl apply -f deployment.yaml
 kubectl get deployment  first-service
+
 #apply service
-kubectl apply -f service.yml
-kubectl get services
+echo "Creating services for first-service"
+kubectl apply -f service.yaml
+kubectl get services first-service
--- second-service/deployment/kubernetes/commands.sh
+++ second-service/deployment/kubernetes/commands.sh
@@ -1,16 +1,29 @@
+#!/bin/bash
+echo "Kubernetes updates ....."
+
 #delete services
+echo "Deleting services for second-service"
 kubectl delete service second-service
+
 #delete deployments
+echo "Deleting deployment for second-service"
 kubectl delete deployment second-service
+
 #delete configmap
+echo "Deleting configmap for second-service"
 kubectl delete configmap second-service
 
 #apply configmap
+echo "Creating  configmap for second-service"
 kubectl apply -f configmap.yaml
 kubectl get configmap second-service
+
 #apply deployment
-kubectl apply -f deployment.yml
+echo "Creating deployment for second-service"
+kubectl apply -f deployment.yaml
 kubectl get deployment  second-service
+
 #apply service
-kubectl apply -f service.yml
-kubectl get services
+echo "Creating services for second-service"
+kubectl apply -f service.yaml
+kubectl get services second-service
