213061226bd419810a0a3d45f2fdb207dbd5a7a7
Nayan Kapri <nrkapri@gmail.com>
2019-08-24 21:28:16 +0000

    update command scripts

--- first-service/deployment/kubernetes/commands.sh
+++ first-service/deployment/kubernetes/commands.sh
@@ -1,4 +1,16 @@
+#delete services
+kubectl delete service first-service
+#delete deployments
+kubectl delete deployment first-service
+#delete configmap
+kubectl delete configmap first-service
+
+#apply configmap
+kubectl apply -f configmap.yaml
+kubectl get configmap first-service
+#apply deployment
 kubectl apply -f deployment.yml
 kubectl get deployment  first-service
+#apply service
 kubectl apply -f service.yml
 kubectl get services
--- second-service/deployment/kubernetes/commands.sh
+++ second-service/deployment/kubernetes/commands.sh
@@ -1,4 +1,16 @@
+#delete services
+kubectl delete service second-service
+#delete deployments
+kubectl delete deployment second-service
+#delete configmap
+kubectl delete configmap second-service
+
+#apply configmap
+kubectl apply -f configmap.yaml
+kubectl get configmap second-service
+#apply deployment
 kubectl apply -f deployment.yml
-kubectl get deployment  first-service
+kubectl get deployment  second-service
+#apply service
 kubectl apply -f service.yml
 kubectl get services
