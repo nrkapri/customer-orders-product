7c8ac3f044b570212aa4b2adf1da64a2fba18b42
Nayan Kapri <nayan.kapri@amdocs.com>
2019-08-24 22:28:48 +0530

    initial commit

---
+++ first-service/deployment/kubernetes/commands.sh
@@ -0,0 +1,3 @@
+kubectl apply -f deployment.yml
+kubectl get deployment  first-service
+kubectl get pods first-service
---
+++ second-service/src/main/java/hello/GreetingController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello second, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	
+    	final String uri = "http://localhost:8080/greeting";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template+result, name));
+    }
+}
