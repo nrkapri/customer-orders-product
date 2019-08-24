80559ed0d578af12910a9b014af7da1340932e61
Nayan Kapri <nrkapri@gmail.com>
2019-08-25 02:41:01 +0530

    kube files

--- first-service/src/main/java/hello/GreetingController.java
+++ first-service/src/main/java/hello/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
